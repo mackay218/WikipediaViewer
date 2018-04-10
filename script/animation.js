07dfa4c43086806089ca94a3fc064ace1c865769
mackay2588 <mackay2588@gmail.com>
2018-04-09 22:43:39 -0500

    size and layout changes

--- script/animation.js
+++ script/animation.js
@@ -2,8 +2,14 @@
 
   var startUpTl = new TimelineLite({paused: true});
 
+  if(window.innerWidth > window.innerHeight){
+    startUpTl.add(TweenLite.to("#searchInput", 0.5, {width: "50vw"}));
+  }
+  else if(window.innerHeight > window.innerWidth){
+    startUpTl.add(TweenLite.to("#searchInput", 0.5, {width: "80vw"}));
+  }
 
-  startUpTl.add(TweenLite.from("#searchInput", 2, {width: "2vw"}));
+
   startUpTl.add(TweenLite.from("#searchBtn", 0.5, {opacity: 0}));
   startUpTl.add(TweenLite.from(".button", 0.5, {opacity: 0}));
 
@@ -11,10 +17,10 @@
 
   var searchTl = new TimelineLite({paused: true});
 
-  searchTl.add(TweenLite.to("#resultsContainer", 0.1, {borderTop: "2px solid black"}));
-  searchTl.add(TweenLite.from("#resultsContainer", 3, {width: 0}));
-  searchTl.add(TweenLite.from("#resultsContainer", 0.1, {border: "0"}));
-  searchTl.add(TweenLite.from("#resultsContainer", 0.5, {height: 0}));
+  searchTl.add(TweenLite.to(".container", 0.1, {borderTop: "2px solid black"}));
+  searchTl.add(TweenLite.from(".container", 3, {width: 0}));
+  searchTl.add(TweenLite.from(".container", 0.1, {border: "0"}));
+  searchTl.add(TweenLite.from(".container", 0.5, {height: 0}));
 
   var logoTl = new TimelineLite({paused: true});
 
